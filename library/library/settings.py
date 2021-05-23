03f3ef2221f3e9c8bf74f2809ef0c3a1561c6fc5
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-05-23 20:44:19 -0300

    Heroku configuration

--- library/library/settings.py
+++ library/library/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path, os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-_p@lc*68(n^g(32bt^%(74&an1&&k_k$-s&#jbo)wkcjk+1v!r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://library-higor.herokuapp.com/']
 
 
 # Application definition
@@ -124,6 +125,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
